d2589cbf9bf166f5b086792b4442d17899cf20a2
allysonjoses <allyson.jose@outlook.com>
2015-03-12 03:34:58 -0300

    ViewModel 85%

---
+++ Poo_Final/Utilities/Backup.cs
@@ -0,0 +1,99 @@
+﻿using Poo_Final.Model;
+using Poo_Final.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Poo_Final.Utilities
+{
+    public class Backup
+    {
+        private static string diretorioArquivo = Directory.GetCurrentDirectory() + @"\backup\backup.xml";
+
+        internal static MainViewModel RestoreBackup(string login)
+        {
+            try
+            {
+                List<MainViewModel> result = Backup.getBackup();
+
+                if (result != null)
+                    return result.FirstOrDefault(r => r.Login.Equals(login));
+            }
+            catch (Exception) { }
+
+            return null;
+        }
+
+        internal static void ExecuteBackup(MainViewModel item)
+        {
+            if (item == null) return;
+
+            try
+            {
+                List<MainViewModel> result = Backup.getBackup();
+
+                if (result != null)
+                {
+                    foreach (var e in result)
+                    {
+                        if (e.Login.Equals(item.Login))
+                        {
+                            e.Items = item.Items;
+                            setBackup(result);
+                            return;
+                        }
+                    }
+
+                    result.Add(item);
+                    setBackup(result);
+                }
+                else
+                {
+                    result = new List<MainViewModel>();
+                    result.Add(item);
+                    setBackup(result);
+                }
+            }
+            catch (Exception) { }
+        }
+
+        private static List<MainViewModel> getBackup()
+        {
+            try
+            {
+                if (File.Exists(diretorioArquivo))
+                {
+                    XmlSerializer deserializer = new XmlSerializer(typeof(List<MainViewModel>));
+                    TextReader textReader = new StreamReader(diretorioArquivo);
+                    List<MainViewModel> result = (List<MainViewModel>)deserializer.Deserialize(textReader);
+                    textReader.Close();
+
+                    return result;
+                }
+                else return null;
+            }
+            catch (Exception) { return null; }
+        }
+
+        private static void setBackup(List<MainViewModel> items)
+        {
+            try
+            {
+                if (!Directory.Exists(Path.GetDirectoryName(diretorioArquivo)))
+                    Directory.CreateDirectory(Path.GetDirectoryName(diretorioArquivo));
+
+                XmlSerializer serializer = new XmlSerializer(typeof(List<MainViewModel>));
+                TextWriter textWriter = new StreamWriter(diretorioArquivo);
+                serializer.Serialize(textWriter, items);
+                textWriter.Close();
+
+            }
+            catch (Exception) { }
+        }
+    }
+}
--- Poo_Final/ViewModel/MainViewModel.cs
+++ Poo_Final/ViewModel/MainViewModel.cs
@@ -2,16 +2,36 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using MahApps.Metro.Controls.Dialogs;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Poo_Final.Utilities;
+using Poo_Final.API;
+using System.Threading;
 
 namespace Poo_Final.ViewModel
 {
     public class MainViewModel : BaseViewModel
     {
+
+        private Publication current;
         private string login;
+        private string hashtag;
+        private string textBtn;
+        private bool editHashtag;
+        private MainWindow mainWindow;
+        private bool visibleLogin;
+        private bool visibleSystem;
+       
+        public ObservableCollection<Publication> Items { get; set; }
+
+        public Publication Current
+        {
+            get { return current; }
+            set { current = value; }
+        }
 
         public string Login
         {
@@ -23,16 +43,115 @@ namespace Poo_Final.ViewModel
             }
         }
 
-        public ObservableCollection<Publication> Items;
-
-        private Publication atual;
-
-        public Publication Atual
+        public string Hashtag
         {
-            get { return atual; }
-            set { atual = value; }
+            get { return hashtag; }
+            set
+            {
+                this.RaisePropertyChanged("Hashtag");
+                hashtag = value;
+            }
         }
-        
+
+        public string TextBtn
+        {
+            get 
+            {
+                if (editHashtag) return "Salvar";
+                else return "Editar";
+            }
+
+        }
+
+        public bool VisibleLogin
+        {
+            get { return visibleLogin; }
+            set
+            {
+                this.RaisePropertyChanged("VisibleLogin");
+                visibleLogin = value;
+            }
+        }
+
+        public bool VisibleSystem
+        {
+            get { return visibleSystem; }
+            set
+            {
+                this.RaisePropertyChanged("VisibleSystem");
+                visibleSystem = value;
+            }
+        }
+
+        public bool EditHashtag
+        {
+            get { return editHashtag; }
+            set
+            {
+                this.RaisePropertyChanged("EditHashtag");
+                editHashtag = value;
+            }
+        }
+
+        public MainViewModel(){}
+        public MainViewModel(MainWindow mainWindow)
+        {
+            this.mainWindow = mainWindow;
+            visibleLogin = true;
+        }
+
+        #region Funcionalidades
+
+        public void ExecuteLogin()
+        {
+            if (String.IsNullOrEmpty(Login)) LoginError();
+            else
+            {
+                var result = Backup.RestoreBackup(Login);
+                if (result != null) Items = result.Items;
+                else Backup.ExecuteBackup(this);
+
+                VisibleLogin = false;
+                VisibleSystem = true;
+
+                Task.Run(() => AlimentarInstagram());
+            }
+        }
+
+        private void AlimentarInstagram()
+        {
+            Instagram api = new Instagram();
+
+            while (true)
+            {
+                try
+                {
+                    if (!EditHashtag)
+                    {
+                        var result = api.GetPublications(hashtag);
+
+                        foreach (var item in result)
+                        {
+                            mainWindow.Dispatcher.Invoke(() =>
+                            {
+                                Items.Add(item);
+                            });
+                        }
+                        
+                        Thread.Sleep(10000);
+                    }
+                }
+                catch (Exception) { Thread.Sleep(20000); }
+            }
+        }
+
+        private async void LoginError()
+        {
+            await mainWindow.ShowMessageAsync("Login inválido!", "Verifique os seus dados e tente novamente!");
+        }
+
+        #endregion
+
 
     }
 }
